8871a523b617fd515f5b8e906d7a4773519bdd16
Martyna Romanowski <martynaromanowski@gmail.com>
2022-06-28 11:30:29 -0400

    Initial Commit

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Martyna Romanowski on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important =  false
+}
---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Martyna Romanowski on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
